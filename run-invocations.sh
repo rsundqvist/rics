2faebd01ba1d2662ef54770fa70fb688a582bd6c
Richard Sundqvist <richard.sundqvist@live.se>
2022-03-13 10:13:47 +0000

    Ensure use of poetry in run-invocations.sh

--- run-invocations.sh
+++ run-invocations.sh
@@ -2,17 +2,17 @@
 
 echo "---------- run-invocations.sh -----------"
 echo "1/7: Clean ------------------------------"
-inv clean
+poetry run inv clean
 echo "2/7: Format code ------------------------"
-inv format
+poetry run inv format
 echo "3/7: Test -------------------------------"
-inv tests
+poetry run inv tests
 echo "4/7: Lint -------------------------------"
-inv lint
+poetry run inv lint
 echo "5/7: Typecheck (mypy) -------------------"
-inv mypy
+poetry run inv mypy
 echo "6/7: Coverage report (open browser) -----"
-inv coverage -f=html -o
+poetry run inv coverage -f=html -o
 echo "7/7: Generate docs (open browser) -------"
-inv docs -o
+poetry run inv docs -o
 echo "---------------- Finished ---------------"
