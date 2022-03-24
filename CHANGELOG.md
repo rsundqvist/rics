bb3c0a9ae18da1cd6626a34179bbe54c73b1d7ce
Richard Sundqvist <richard.sundqvist@live.se>
2022-03-24 19:43:32 +0000

    Add plotting config utilities

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 
 ## [Unreleased]
+### Added
+- The `rics.utility.plotting` module
 
 ## [0.3.2] - 2022-03-17
 
---
+++ src/rics/utility/_just_the_way_i_like_it.py
@@ -0,0 +1,26 @@
+import logging
+from typing import Union
+
+
+def configure_stuff(
+    level: Union[int, str] = logging.INFO, matplotlib_log_level: Union[int, str] = logging.WARNING
+) -> None:
+    """Configure a bunch of stuff to match my personal preferences.
+
+    Caveat Emptor: May do strange stuff :).
+
+    Args:
+        level: Root log level.
+        matplotlib_log_level: Matplotlib log level.
+
+    See Also:
+        Methods used:
+
+            * :meth:`rics.utility.logs.basic_config`
+            * :meth:`rics.utility.plotting.configure`
+    """
+    from rics.utility.logs import basic_config
+    from rics.utility.plotting import configure
+
+    basic_config(level=level, matplotlib_log_level=matplotlib_log_level)
+    configure()
