b7a32953cde3fdd7bbc16162661234bcd2a49ecf
Richard Sundqvist <richard.sundqvist@live.se>
2022-06-13 23:57:36 +0000

    Replace dagger sign that doesn't render properly in Windows test bench

--- tests/translation/dio/test_equality_between_types.py
+++ tests/translation/dio/test_equality_between_types.py
@@ -20,16 +20,16 @@ UNTRANSLATED = {
 TRANSLATED = {
     "firstTitle": [
         "3 not translated; default name=Title unknown",
-        "41064:Think Fast (original: Think Fast) *1949†1950",
+        "41064:Think Fast (original: Think Fast) from: 1949, to: 1950",
         "15 not translated; default name=Title unknown",
-        "41068:Versatile Varieties (original: Versatile Varieties) *1949†1951",
-        "41069:The Voice of Firestone (original: The Voice of Firestone) *1949†1963",
+        "41068:Versatile Varieties (original: Versatile Varieties) from: 1949, to: 1951",
+        "41069:The Voice of Firestone (original: The Voice of Firestone) from: 1949, to: 1963",
     ],
     "nconst": [
-        "1:Fred Astaire *1899†1987",
-        "15:James Dean *1931†1955",
+        "1:Fred Astaire from: 1899, to: 1987",
+        "15:James Dean from: 1931, to: 1955",
         "500 not translated; default name=Name unknown",
-        "30:Audrey Hepburn *1929†1993",
-        "50:Groucho Marx *1890†1977",
+        "30:Audrey Hepburn from: 1929, to: 1993",
+        "50:Groucho Marx from: 1890, to: 1977",
     ],
 }
