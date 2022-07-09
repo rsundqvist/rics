bd4c2f0ebe4af6ced36221819d952c37f2d3a04e
Richard Sundqvist <richard.sundqvist@live.se>
2022-07-09 16:47:19 +0000

    Implement override functions in Mapper.apply

--- src/rics/mapping/exceptions.py
+++ src/rics/mapping/exceptions.py
@@ -5,12 +5,20 @@ class MappingError(ValueError):
     """Something failed to map."""
 
 
+class UserMappingError(ValueError):
+    """A user-defined mapping function did something strange."""
+
+
 class CardinalityError(MappingError):
     """Base class for cardinality issues."""
 
 
 class MappingWarning(UserWarning):
     """Something failed to map."""
+
+
+class UserMappingWarning(MappingWarning):
+    """A user-defined mapping function did something strange."""
 
 
 class BadFilterError(MappingError):
