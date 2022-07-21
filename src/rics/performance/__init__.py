cdcad4c7dc53467c70e252321d16d89155d76435
Richard Sundqvist <richard.sundqvist@live.se>
2022-07-21 17:23:07 +0000

    Rename _wrappers.py -> _wrapper.py

--- src/rics/performance/__init__.py
+++ src/rics/performance/__init__.py
@@ -3,7 +3,7 @@
 from ._format_perf_counter import format_perf_counter, format_seconds
 from ._multi_case_timer import MultiCaseTimer
 from ._util import plot_run, to_dataframe
-from ._wrappers import run_multivariate_test
+from ._wrapper import run_multivariate_test
 
 __all__ = [
     "MultiCaseTimer",
