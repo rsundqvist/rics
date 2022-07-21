3cea687ae0b849bfe0489c572a1cd9c00ff7b787
Richard Sundqvist <richard.sundqvist@live.se>
2022-07-21 17:21:47 +0000

    Add format_seconds method and increase high-precision threshold from 60 to 120 sec

--- src/rics/performance/__init__.py
+++ src/rics/performance/__init__.py
@@ -1,6 +1,6 @@
 """Performance testing utility."""
 
-from ._format_perf_counter import format_perf_counter
+from ._format_perf_counter import format_perf_counter, format_seconds
 from ._multi_case_timer import MultiCaseTimer
 from ._util import plot_run, to_dataframe
 from ._wrappers import run_multivariate_test
@@ -9,6 +9,7 @@ __all__ = [
     "MultiCaseTimer",
     "run_multivariate_test",
     "format_perf_counter",
+    "format_seconds",
     "plot_run",
     "to_dataframe",
 ]
