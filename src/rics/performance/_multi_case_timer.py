c2340c7692f2896e739ab738dfb28248c08db032
Richard Sundqvist <richard.sundqvist@live.se>
2022-07-22 17:03:13 +0000

    Fix niter computation in MultiCaseTimer

--- src/rics/performance/_multi_case_timer.py
+++ src/rics/performance/_multi_case_timer.py
@@ -3,7 +3,6 @@ import warnings
 from timeit import Timer
 from typing import Any, Callable, Collection, Dict, List, Optional, Union
 
-from rics.performance._format_perf_counter import format_perf_counter
 from rics.performance._format_perf_counter import format_seconds as fmt_time
 from rics.utility.misc import tname
 
@@ -19,19 +18,12 @@ class MultiCaseTimer:
     Args:
         candidate_method: A single method, collection of method or a dict {label: function} of candidates.
         test_data: A single datum or a dict ``{label: data}`` to evaluate candidates on.
-        sanity_check: If ``True``, verify total expected runtime.
     """
-
-    EXPECTED_RUNTIME_WARNING_LIMIT = 4 * 60 * 60
-    """Warn of long runtimes above this limit (4 hours)."""
-    EXPECTED_RUNTIME_ERROR_LIMIT: int = 6 * EXPECTED_RUNTIME_WARNING_LIMIT
-    """Raise an exceptions above this limit (six times the ``EXPECTED_RUNTIME_WARNING_LIMIT``)."""
 
     def __init__(
         self,
         candidate_method: Union[CandFunc, Collection[CandFunc], Dict[str, CandFunc]],
         test_data: Union[Any, Dict[str, Any]],
-        sanity_check: bool = True,
     ) -> None:
         self._candidates = _process_candidates(candidate_method)
         if not self._candidates:  # pragma: no cover
@@ -40,7 +32,6 @@ class MultiCaseTimer:
         self._data = test_data if isinstance(test_data, dict) else _process_single_test_datum(test_data)
         if not self._data:  # pragma: no cover
             raise ValueError("No case data given.")
-        self._check = sanity_check
 
     def run(
         self,
@@ -75,15 +66,13 @@ class MultiCaseTimer:
         See Also:
             The :py:class:`timeit.Timer` class which this implementation depends on.
         """
-        per_candidate_number = self._compute_number_of_iterations(number, time_per_candidate)
-        if self._check and number is None:  # pragma: no cover
-            self._sanity_check(repeat, time_per_candidate, self.EXPECTED_RUNTIME_ERROR_LIMIT)
+        per_candidate_number = self._compute_number_of_iterations(number, repeat, time_per_candidate)
 
         run_results = {}
         for candidate_label, func in self._candidates.items():
             candidate_number = per_candidate_number[candidate_label]
             candidate_results: Dict[str, List[float]] = {}
-            LOGGER.info("Evaluate candidate '%s' %dx%d times...", candidate_label, repeat, candidate_number)
+            LOGGER.info("Evaluate candidate '%s' %dx%d times..", candidate_label, repeat, candidate_number)
             for data_label, test_data in self._data.items():
                 raw_timings = Timer(lambda: func(test_data)).repeat(repeat, candidate_number)  # noqa: B023
                 best, worst = min(raw_timings), max(raw_timings)
@@ -101,7 +90,7 @@ class MultiCaseTimer:
         return run_results
 
     def _compute_number_of_iterations(
-        self, number: Optional[int], time_allocation: float
+        self, number: Optional[int], repeat: int, time_allocation: float
     ) -> Dict[str, int]:  # pragma: no cover
         if isinstance(number, int):
             ans = {case: number for case in self._data}
@@ -113,28 +102,11 @@ class MultiCaseTimer:
                     for data in self._data.values():
                         candidate_func(data)  # noqa: B023
 
-                number, time_taken = Timer(_run_all).autorange()
-                ans[candidate_label] = max(2, int(number * time_allocation / time_taken))
+                auto_number, auto_time = Timer(_run_all).autorange()
+                candidate_number = int((time_allocation * auto_number) / (repeat * auto_time))
+                ans[candidate_label] = max(2, candidate_number)
 
         return ans
-
-    def _sanity_check(
-        self, repeat: int, time_allocation: float, max_expected_runtime: Optional[float]
-    ) -> None:  # pragma: no cover
-        expected_runtime = len(self._candidates) * repeat * time_allocation
-
-        ert_nice = format_perf_counter(0, expected_runtime)
-        if max_expected_runtime is not None and expected_runtime > self.EXPECTED_RUNTIME_WARNING_LIMIT:
-            warnings.warn(f"Long total expected runtime: {ert_nice}")
-
-        if max_expected_runtime is not None and expected_runtime > max_expected_runtime:
-            limit = format_perf_counter(0, max_expected_runtime)
-            raise ValueError(
-                f"Long expected runtime: {ert_nice} > limit={limit}. Increase 'max_expected_runtime' or"
-                " set it to None to allow."
-            )
-
-        LOGGER.info(f"Expected total runtime: {ert_nice}.")
 
 
 def _process_candidates(
@@ -150,5 +122,5 @@ def _process_candidates(
 
 def _process_single_test_datum(test_data: Any) -> Dict[str, Any]:  # pragma: no cover
     s = repr(test_data)
-    key = f"{s[:32]}..." if len(s) > 32 else s
-    return {f"Example: '{key}'": test_data}
+    key = f"{s[:29]}..." if len(s) > 32 else s
+    return {f"Sample data: '{key}'": test_data}
