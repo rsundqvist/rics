61ea3d80a040a9b100368ebe91d095ce28aaf929
Richard Sundqvist <richard.sundqvist@live.se>
2022-08-01 22:09:39 +0000

    Bump version: 0.14.0 → 0.14.0.dev1

--- src/rics/__version__.py
+++ src/rics/__version__.py
@@ -2,6 +2,6 @@
 
 __title__ = "rics"
 __description__ = "My personal little ML engineering library."
-__version__ = "0.14.0"
+__version__ = "0.14.0.dev1"
 __author__ = "Richard Sundqvist"
 __copyright__ = "2022, Richard Sundqvist"
