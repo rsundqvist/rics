3e26efb9439521a40b063285d2b42be925ddc19c
Richard Sundqvist <richard.sundqvist@live.se>
2022-10-05 20:26:45 +0200

    Refactor multicase perftest CLI
    - Ability for the perftest CLI program to create and run dummy tests.
    - Renamed cli program to `mtimeit` (was: `rics-perf`)
    - Moved CLI functionality into the new `cli` extra

--- tests/performance/test_wrapper.py
+++ tests/performance/test_wrapper.py
@@ -1,41 +1,86 @@
-from dataclasses import dataclass
-from time import sleep
+import sys
+from pathlib import Path
 
+import pandas as pd
 import pytest
+from click.testing import CliRunner
 
-from rics.performance import MultiCaseTimer, run_multivariate_test
+from rics.performance import MultiCaseTimer, cli, run_multivariate_test
 
 
-@dataclass(frozen=True)
-class CandidateFunction:
-    sleep_multiplier: int
+def unload_modules():
+    for key in list(filter(lambda s: "cli_modules" in s or "test_data" in s or "candidates" in s, sys.modules)):
+        del sys.modules[key]
 
-    def __call__(self, time):
-        sleep(time * self.sleep_multiplier)
+
+@pytest.mark.filterwarnings("ignore:The test results may be unreliable:UserWarning")
+@pytest.mark.filterwarnings("ignore:Matplotlib is currently using agg:UserWarning")
+def test_cli_create():
+    unload_modules()
+
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        result = runner.invoke(
+            cli.main,
+            ["--time-per-candidate=0.01", "--create", "--name=create-unit-test"],
+            catch_exceptions=False,
+        )
+    assert result.exit_code == 0
 
 
 def get_raw_timings(self, func, test_data, repeat, number):
     return [func.sleep_multiplier * test_data] * repeat
 
 
-MultiCaseTimer._get_raw_timings = get_raw_timings  # type: ignore
+@pytest.mark.parametrize("with_all", (False, True))
+@pytest.mark.filterwarnings("ignore:Matplotlib is currently using agg:UserWarning")
+def test_cli(monkeypatch, with_all):
+    monkeypatch.setattr(MultiCaseTimer, "_get_raw_timings", get_raw_timings)
+    unload_modules()
+
+    if with_all:
+        from tests.performance.cli_modules.with_all import candidates, test_data  # type: ignore
+    else:
+        from tests.performance.cli_modules.without_all import candidates, test_data  # type: ignore
+
+    runner = CliRunner()
+    with runner.isolated_filesystem() as tmpdir:
+        with open(candidates.__file__) as f:
+            Path(tmpdir).joinpath("candidates.py").write_text(f.read())
+        with open(test_data.__file__) as f:
+            Path(tmpdir).joinpath("test_data.py").write_text(f.read())
+
+        result = runner.invoke(
+            cli.main,
+            ["--time-per-candidate=0.1", "--name=unit-test"],
+            catch_exceptions=False,
+        )
+        assert result.exit_code == 0
+        assert Path(tmpdir).joinpath("unit-test.png").is_file()
+        csv = Path(tmpdir).joinpath("unit-test.csv")
+        assert csv.is_file()
+        verify_ans(pd.read_csv(csv))
 
 
 @pytest.mark.filterwarnings("ignore:Matplotlib is currently using agg:UserWarning")
-def test_run_multivariate_test():
+def test_run_multivariate_test(monkeypatch):
+    monkeypatch.setattr(MultiCaseTimer, "_get_raw_timings", get_raw_timings)
+    unload_modules()
+
+    from tests.performance.cli_modules.with_all import candidates, test_data
+
+    MultiCaseTimer._get_raw_timings = get_raw_timings  # type: ignore
+
     ans = run_multivariate_test(
-        candidate_method={
-            "sleep": CandidateFunction(1),
-            "sleep_x4": CandidateFunction(4),
-        },
-        test_data={
-            "1 ms": 0.001,
-            "3 ms": 0.003,
-        },
+        candidate_method=candidates.ALL,
+        test_data=test_data.ALL,
         time_per_candidate=0.1,
     )
+    verify_ans(ans)
+
+
+def verify_ans(ans):
     assert sorted(ans["Candidate"].unique()) == ["sleep", "sleep_x4"]
     assert sorted(ans["Test data"].unique()) == ["1 ms", "3 ms"]
-
     best = ans.groupby(["Candidate", "Test data"])["Time [ms]"].min()
     assert (best["sleep"] < best["sleep_x4"]).all()
