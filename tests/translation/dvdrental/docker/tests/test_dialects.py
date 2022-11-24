2295264148aa2b7065367c88251e730ff7ac1313
Richard Sundqvist <richard.sundqvist@live.se>
2022-11-24 23:14:33 +0100

    Add preloaded Sakila / dvdrental images
    See: https://hub.docker.com/r/rsundqvist/sakila-preload

---
+++ tests/translation/dvdrental/docker/tests/test_dialects.py
@@ -0,0 +1,43 @@
+# noqa
+
+from pathlib import Path
+
+import pandas as pd
+import pytest
+import sqlalchemy
+import yaml  # type: ignore
+
+DIR = Path(__file__).parent
+CREDENTIALS = yaml.safe_load(DIR.parent.joinpath("credentials.yml").read_text())["dialects"]
+DIALECTS = list(CREDENTIALS)
+CONNECTION_STRING = "{dialect}+{driver}://{user}:{password}@localhost:{port}/{database}"
+
+QUERY = DIR.joinpath("query.sql").read_text()
+DRIVERS = {
+    "mysql": "pymysql",
+    "postgresql": "pg8000",
+    "mssql": "pymssql",
+}
+
+
+def execute_query(dialect: str) -> pd.DataFrame:
+    connection_string = CONNECTION_STRING.format(dialect=dialect, driver=DRIVERS[dialect], **CREDENTIALS[dialect])
+    return pd.read_sql(QUERY, sqlalchemy.create_engine(connection_string))
+
+
+def test_reference() -> None:
+    actual = execute_query("mysql")
+    assert actual.shape == (16044, 6)
+
+    expected = pd.read_csv(DIR.joinpath("expected.csv"), index_col=0, parse_dates=["rental_date", "return_date"])
+    pd.testing.assert_frame_equal(actual.loc[expected.index], expected)
+
+
+@pytest.mark.parametrize("dialect", DIALECTS)
+def test_equality(dialect: str, expected: pd.DataFrame) -> None:
+    pd.testing.assert_frame_equal(execute_query(dialect), expected)
+
+
+@pytest.fixture(scope="session")
+def expected() -> pd.DataFrame:
+    return execute_query("mysql")
