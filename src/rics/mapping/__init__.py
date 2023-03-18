e3e1604d4975afec399d4dfaccdc5ae27a338f01
Richard Sundqvist <richard.sundqvist@live.se>
2023-03-18 12:42:07 +0100

    Deprecate rics.mapping

--- src/rics/mapping/__init__.py
+++ src/rics/mapping/__init__.py
@@ -3,6 +3,8 @@
 For and introduction to mapping, see :ref:`mapping-primer`.
 """
 
+from warnings import warn as _warn
+
 from ._cardinality import Cardinality
 from ._directional_mapping import DirectionalMapping
 from ._heuristic_score import HeuristicScore
@@ -14,3 +16,5 @@ __all__ = [
     "DirectionalMapping",
     "Mapper",
 ]
+
+_warn("rics.mapping has been deprecated and will be removed in 4.0", category=PendingDeprecationWarning)
