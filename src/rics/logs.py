738d71b63781544d191aa79da11346fd9ebc8b07
Richard Sundqvist <richard.sundqvist@live.se>
2023-08-27 17:18:36 +0200

    Bugfixes
    - Improve parsing of env var strings
    - Handle LoggerAdapter in logs.disable_temporarily()
    
    Bonus content: _level-suffixes are back!

--- src/rics/logs.py
+++ src/rics/logs.py
@@ -1,7 +1,7 @@
 """Utility methods for logging tasks."""
 import logging
 from contextlib import contextmanager as _contextmanager
-from typing import Any, Union
+from typing import Any, Dict, List, Tuple, Union
 
 FORMAT: str = "%(asctime)s.%(msecs)03d [%(name)s:%(levelname)s] %(message)s"
 """Default logging format; ``<date-format>.378 [rics:DEBUG] I'm a debug message!``"""
@@ -15,9 +15,6 @@ def basic_config(
     format: str = FORMAT,  # noqa: A002
     datefmt: str = DATE_FORMAT,
     level: Union[int, str] = logging.INFO,
-    rics_level: Union[int, str] = logging.INFO,
-    id_translation_level: Union[int, str] = logging.WARNING,
-    matplotlib_level: Union[int, str] = logging.WARNING,
     force: bool = True,
     **kwargs: Any,
 ) -> None:
@@ -29,33 +26,35 @@ def basic_config(
         format: Format string for emitted messages; see :attr:`FORMAT`.
         datefmt: Format string for date/time; see :attr:`DATE_FORMAT`.
         level: Log level for the root logger. Default is ``logging.INFO``.
-        rics_level: Log level for the :mod:`rics` package. Default is ``logging.INFO``.
-        id_translation_level: Log level for the :mod:`id_translation` package. Default is ``logging.WARNING``.
-        matplotlib_level: Log level for the :mod:`matplotlib` package. Default is ``logging.WARNING``.
         force: If ``True``, override existing configuration if it exists.
         **kwargs: Keyword arguments for :py:func:`logging.basicConfig`.
+
+    Keyword Args:
+        _level: Additional log levels to set, replacing *double* underscores with dots to produce the logger names. For
+            example, passing ``rics__performance_level=logging.INFO`` will modify the `'rics.performance'`-logger.
 
     Examples:
         Basic usage.
 
         >>> from rics.logs import basic_config, logging
-        >>> root_logger = logging.getLogger()
         >>> basic_config(level=logging.INFO, rics_level=logging.DEBUG)
         >>> logging.getLogger("rics").debug("I'm a debug message!")
-        >>> root_logger.debug("I'm a debug message!")
-        >>> root_logger.critical("I'm a critical message!") # Doctest: +SKIP
+        >>> logging.debug("I'm a debug message!")
+        >>> logging.critical("I'm a critical message!") # Doctest: +SKIP
         2022-02-05T11:17:05.378 [rics:DEBUG] I'm a debug message!
         2022-02-05T11:17:05.378 [root:CRITICAL] I'm a critical message!
     """
+    extra_levels, kwargs = _extract_extra_levels(**kwargs)
     logging.basicConfig(level=level, format=format, datefmt=datefmt, force=force, **kwargs)
-    logging.getLogger("rics").setLevel(rics_level)
-    logging.getLogger("id_translation").setLevel(id_translation_level)
-    logging.getLogger("matplotlib").setLevel(matplotlib_level)
+
+    for name, level in extra_levels.items():
+        logging.getLogger(name).setLevel(level)
 
 
 @_contextmanager
 def disable_temporarily(  # type: ignore[no-untyped-def]
-    logger: logging.Logger, *more_loggers: logging.Logger
+    logger: Union[logging.Logger, logging.LoggerAdapter],  # type:  ignore[type-arg]
+    *more_loggers: Union[logging.Logger, logging.LoggerAdapter],  # type:  ignore[type-arg]
 ):  # noqa: ANN201
     """Temporarily disable logging.
 
@@ -74,7 +73,12 @@ def disable_temporarily(  # type: ignore[no-untyped-def]
         >>> with disable_temporarily(logging.root):
         ...     logging.info("This message is ignored.")
     """
-    loggers = [logger] + list(more_loggers)
+    loggers: List[logging.Logger] = []
+    for lgr in (logger, *more_loggers):
+        while isinstance(lgr, logging.LoggerAdapter):
+            lgr = lgr.logger
+        loggers.append(lgr)
+
     states = [lgr.disabled for lgr in loggers]
 
     try:
@@ -125,3 +129,18 @@ def logger_from_object(obj: Any, prefix_class: bool = True) -> logging.Logger:
         parts.append(part)
     parts.append(tname(obj, prefix_classname=prefix_class))
     return logging.getLogger(".".join(parts))
+
+
+def _extract_extra_levels(**kwargs: Any) -> Tuple[Dict[str, Union[int, str]], Dict[str, Any]]:
+    suffix = "_level"
+
+    levels: Dict[str, Union[int, str]] = {}
+    for key in list(kwargs.keys()):
+        if key and key.endswith(suffix):
+            wildcard_key = key[: -len(suffix)].replace("__", ".")
+
+            level = kwargs.pop(key)
+            if level is not None:  # pragma: no cover
+                levels[wildcard_key] = level
+
+    return levels, kwargs
--- tests/test_logs.py
+++ tests/test_logs.py
@@ -2,7 +2,7 @@ import logging
 
 import pytest
 
-from rics.logs import basic_config
+from rics.logs import _extract_extra_levels, basic_config, disable_temporarily
 
 
 @pytest.mark.skip(reason="Messes up logging for other tests.")
@@ -22,3 +22,45 @@ def test_set_levels(logger_name, level_name):
 
     expected_level = logging.getLevelName(level_name)
     assert logging.getLogger(logger_name).getEffectiveLevel() == expected_level
+
+
+@pytest.mark.parametrize(
+    "loggers",
+    [
+        [logging.root],
+        [logging.LoggerAdapter(logging.root, extra={})],
+        [logging.LoggerAdapter(logging.getLogger("rics"), extra={})],
+        [logging.LoggerAdapter(logging.getLogger("rics"), extra={}), logging.getLogger("id_translation")],
+    ],
+)
+def test_disable_temporarily(loggers, caplog):
+    def log_messages():
+        caplog.clear()
+        for i, lgr in enumerate(loggers):
+            lgr.warning("Hello from logger #%i of type %r!", i, type(lgr).__name__)
+        return caplog.records
+
+    actual = log_messages()
+    expected = max(1, len(loggers))
+    assert len(actual) == expected
+
+    with disable_temporarily(*loggers):
+        actual = log_messages()
+
+    assert len(actual) == 0
+
+
+@pytest.mark.parametrize(
+    "arg, expected",
+    [
+        # Use ':' instead of '.' in expected to get prettier pytest output.
+        ("_foo___bar", "_foo:_bar"),
+        ("rics__performance", "rics:performance"),
+        ("id_translation", "id_translation"),
+        ("id_translation__fetching", "id_translation:fetching"),
+        ("rics___internal_support", "rics:_internal_support"),
+    ],
+)
+def test_extract_extra_levels(arg, expected):
+    actual = list(_extract_extra_levels(**{f"{arg}_level": logging.INFO})[0])[0]
+    assert actual == expected.replace(":", ".")
